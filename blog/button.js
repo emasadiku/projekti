a93d43039d7867e550d7665aa1c47255f47d1fc5
emasadiku <emasadiku01@gmail.com>
2025-01-13 18:24:21 +0100

    Delete blog directory

--- blog/button.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const button = document.querySelector('.btn-primary');
-    if (button) {
-        button.addEventListener('click', function (event) {
-            event.preventDefault();
-            alert('Thank you for contacting us');
-        });
-    }
-});
